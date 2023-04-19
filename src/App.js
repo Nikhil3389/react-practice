a560b10a77dafd7e59397cb436ddbaffefc1ca6b
Nikhil3389 <nikhil3389@gmail.com>
2023-04-20 00:19:20 +0530

    updating names with state class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import logo from './logo.svg';
-import React from 'react';
-import { useState } from 'react';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  const [data,setData] = useState("Nikhil")
-  function updateData()
+class App extends Component
+{
+  constructor()
   {
-    setData("Kataria")
+    super();
+    this.state={
+      data :"Nikhil"
+    }
   }
+  apple()
+  {
+    this.setState({data:"Kataria"})
+
+  }
+
+render()
+{
   return (
     <div className="App">
-      <h1>{data}</h1>
-      <button onClick={updateData}>Click Me</button>
-     
+      <h1>{this.state.data}</h1>
+      <button onClick={() => this.apple()}>Click Me</button>  
     </div>
   );
+
 }
+  
+}
+  
+
 
 export default App;
---
+++ src/app1.js
@@ -0,0 +1,32 @@
+// import logo from './logo.svg';
+// import React from 'react';
+// import { useState } from 'react';
+// import './App.css';
+
+// function App() {
+//   // const [data,setData] = useState("Nikhil")
+//   const [data,setData] = useState(0)
+  
+//   function updateData()
+//   {
+//     // setData("Kataria")
+//     setData(data+1)
+//   }
+//   const [sub,setSub] = useState(data)
+//   function updateDa()
+//   {
+//     // setData("Kataria")
+//     setSub(sub-1)
+//   }
+//   return (
+//     <div className="App">
+//       <h1>{data}</h1>
+//       <button onClick={updateData}>Click Me</button>
+//        <h1>{sub}</h1>
+//       <button onClick={updateDa}>Click</button>
+     
+//     </div>
+//   );
+// }
+
+// export default App;
