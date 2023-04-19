9000906ad27ce5068e4fa885247d4cfabcf40ed7
Nikhil3389 <nikhil3389@gmail.com>
2023-04-19 22:53:53 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data,setData] = useState("Nikhil")
+  function updateData()
+  {
+    setData("Kataria")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data}</h1>
+      <button onClick={updateData}>Click Me</button>
+     
     </div>
   );
 }
