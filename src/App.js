aa59bd11329ebba67c0a673df53180ce1672de35
Nikhil3389 <nikhil3389@gmail.com>
2023-04-20 10:55:28 +0530

    inputing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import React, { Component, useDeferredValue } from 'react';
+import React, { Component, useDeferredValue, useState } from 'react';
 
 import './App.css';
 import Students from './components/Students';
@@ -41,40 +41,69 @@ import Students from './components/Students';
 // function App()
 // {
 //   return(
-    // <div className="App">
-    //   <h1>Hello</h1>
-    //   <Students name ={"Nikhil"} email={"abc@gmail.com"}/>
-    //   {/* <Students email ={"abc@gmail.com"}/> */}
-    // </div>
+//     <div className="App">
+//       <h1>Hello</h1>
+//       <Students name ={"Nikhil"} email={"abc@gmail.com"}/>
+//       {/* <Students email ={"abc@gmail.com"}/> */}
+//     </div>
 //   )
 // }
 // export default App;
 
 
+// ############# class props #################
+// export default class App extends Component
 
-export default class App extends Component
+// {
+
+
+//   constructor()
+//   {
+//     super();
+//     this.state={
+//       name: "NIKHIL"
+//     }
+//   }
+//   render()
+//   {
+//     return(
+//          <div className="App">
+//       <h1>Hello</h1>
+//       <Students name ={this.state.name}/>
+//       <button onClick={() => this.setState({name:"Kataria"})}>Update Name</button>
+//       {/* <Students email ={"abc@gmail.com"}/> */}
+//     </div>
+
+
+//     )
+//   }
+// }
+
+
+function App()
 
 {
-
-
-  constructor()
+  const [data,setData] = useState(null)
+  const [print,setPrint] =  useState(false)
+  function getData(val)
   {
-    super();
-    this.state={
-      name: "NIKHIL"
-    }
+    console.log(val.target.value)
+    setData(val.target.value)
+    setPrint(false)
   }
-  render()
-  {
-    return(
-         <div className="App">
-      <h1>Hello</h1>
-      <Students name ={this.state.name}/>
-      <button onClick={() => this.setState({name:"Kataria"})}>Update Name</button>
+  return(
+    <div className="App">
+      {
+        print?
+        <h1>Hello</h1>
+        :
+        null
+      }
+      
+      <input type="text" onChange={getData} />
+      <button onClick={() => setPrint(true)}>Print Data</button>
       {/* <Students email ={"abc@gmail.com"}/> */}
     </div>
-
-
-    )
-  }
+  )
 }
+export default App;
