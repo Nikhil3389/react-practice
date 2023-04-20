2b7c4a98b2052139e6b29d49859d1f3e72e94290
Nikhil3389 <nikhil3389@gmail.com>
2023-04-20 09:43:12 +0530

    updation of props

--- src/App.js
+++ src/App.js
@@ -2,35 +2,47 @@ import logo from './logo.svg';
 import React, { Component } from 'react';
 
 import './App.css';
+import Students from './components/Students';
+// adding using state class
 
-class App extends Component
+
+// class App extends Component
+// {
+//   constructor()
+//   {
+//     super();
+//     this.state={
+//       data :"Nikhil"
+//     }
+//   }
+//   apple()
+//   {
+//     this.setState({data:"Kataria"})
+
+//   }
+
+// render()
+// {
+//   return (
+//     <div className="App">
+//       <h1>{this.state.data}</h1>
+//       <button onClick={() => this.apple()}>Click Me</button>  
+//     </div>
+//   );
+
+// }
+  
+// }
+  
+
+function App()
 {
-  constructor()
-  {
-    super();
-    this.state={
-      data :"Nikhil"
-    }
-  }
-  apple()
-  {
-    this.setState({data:"Kataria"})
-
-  }
-
-render()
-{
-  return (
+  return(
     <div className="App">
-      <h1>{this.state.data}</h1>
-      <button onClick={() => this.apple()}>Click Me</button>  
+      <h1>Hello</h1>
+      <Students name ={"Nikhil"} email={"abc@gmail.com"}/>
+      {/* <Students email ={"abc@gmail.com"}/> */}
     </div>
-  );
-
+  )
 }
-  
-}
-  
-
-
 export default App;
---
+++ src/components/Students.js
@@ -0,0 +1,10 @@
+function Students(props)
+{
+    return(
+        <div>
+          <h1> Name:  {props.name}</h1> 
+          <h2> Email:  {props.email}</h2> 
+        </div>
+    )
+}
+export default Students
