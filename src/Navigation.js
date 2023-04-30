83c9b29b7c3a7f0ff27590ea82c06a48baffddf7
Nikhil3389 <nikhil3389@gmail.com>
2023-04-30 12:00:51 +0530

    Error 404

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,12 +2,14 @@ import React from "react";
 import {Routes,Route} from "react-router-dom"
 import About from "./About";
 import Home from "./Home";
+import PageNot from "./PageNot";
 function Navigation(){
     return (
       <div>
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/" exact={true} element={<Home/>} />
+          <Route path="*"  element={<PageNot/>} />
         </Routes>
       </div>
     );
---
+++ src/PageNot.js
@@ -0,0 +1,9 @@
+function PageNot(){
+    return (
+        <div>
+            <h1>Error 404</h1>
+            <p> Page Not Found</p>
+        </div>
+    )
+}
+export default PageNot
